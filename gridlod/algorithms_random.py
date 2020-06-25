7b992465556fe348612e501c134aef17f57022d1
BarbaraV <barbara.verfuerth@math.uni-augsburg.de>
2020-06-25 11:47:46 +0200

    some changes in test

--- gridlod/algorithms_random.py
+++ gridlod/algorithms_random.py
@@ -7,7 +7,8 @@ from gridlod.build_coefficient import build_checkerboardbasis
 
 
 def computeCSI_offline(world, NepsilonEelement, alpha, beta, k, boundaryConditions):
-    middle = world.NWorldCoarse[1] // 2 * world.NWorldCoarse[0] + world.NWorldCoarse[0] // 2 #2d!!!
+    #middle = world.NWorldCoarse[1] // 2 * world.NWorldCoarse[0] + world.NWorldCoarse[0] // 2 #2d!!!
+    middle = NCoarse[0] //2
     patch = PatchPeriodic(world, k, middle)
 
     aRefList = build_checkerboardbasis(patch.NPatchCoarse, NepsilonEelement, world.NCoarseElement, alpha, beta)
@@ -71,15 +72,17 @@ def compute_combined_MsStiffness(world,aPert, aRefList, KmsijList,k):
 from gridlod.world import World
 from gridlod import util, fem, coef
 from gridlod.build_coefficient import build_randomcheckerboard
+import scipy.sparse as sparse
+import matplotlib.pyplot as plt
 
-NFine = np.array([256,256])
+NFine = np.array([256]) #,64
 NpFine = np.prod(NFine+1)
-Nepsilon = np.array([32,32])
-NCoarse = np.array([8,8])
+Nepsilon = np.array([32]) #,16
+NCoarse = np.array([16]) #,8
 k=2
-NSamples = 10
+NSamples = 50
 
-boundaryConditions = np.array([[0, 0], [0, 0]])
+boundaryConditions = None #np.array([[0, 0], [0, 0]])
 alpha = 0.1
 beta = 1.
 
@@ -87,7 +90,7 @@ NCoarseElement = NFine // NCoarse
 world = World(NCoarse, NCoarseElement, boundaryConditions)
 
 xpFine = util.pCoordinates(NFine)
-ffunc = lambda x: 8*np.pi**2*np.sin(2*np.pi*x[:,0])*np.cos(2*np.pi*x[:,1])
+ffunc = lambda x: 8*np.pi**2*np.sin(2*np.pi*x) #[:,0]*np.cos(2*np.pi*x[:,1])
 f = ffunc(xpFine).flatten()
 
 aRefList, KmsijList = computeCSI_offline(world, Nepsilon // NCoarse, alpha, beta, k, boundaryConditions)
@@ -111,8 +114,50 @@ for N in range(NSamples):
     bFull = basis.T * MFull * f
     faverage = np.dot(MFull * np.ones(NpFine), f)
 
+    #fine FEM
+    '''KFEM = fem.assemblePatchMatrix(world.NWorldFine, world.ALocFine,aPert)
+    bFine = MFull*f
+    #make the matrix periodic
+    KFEM.tolil()
+    KFEM[np.arange(0, NFine[1]*(NFine[0]+1)+1, NFine[0]+1),:] \
+        += KFEM[np.arange(NFine[0], np.prod(NFine+1), NFine[0]+1),:]
+    KFEM[:, np.arange(0, NFine[1] * (NFine[0] + 1) + 1, NFine[0] + 1)] \
+        += KFEM[:, np.arange(NFine[0], np.prod(NFine + 1), NFine[0] + 1)]
+    KFEM[np.arange(NFine[0]+1), :] += KFEM[np.arange(NFine[1]*(NFine[0]+1), np.prod(NFine+1)), :]
+    KFEM[:, np.arange(NFine[0] + 1)] += KFEM[:, np.arange(NFine[1] * (NFine[0] + 1), np.prod(NFine + 1))]
+    KFEM.tocsc()
+    averageVector = MFull * np.ones(np.prod(NFine+1))
+    fixed = np.concatenate((np.arange(NFine[1] * (NFine[0] + 1), NpFine),
+                            np.arange(NFine[0], NpFine - 1, NFine[0] + 1)))
+    free = np.setdiff1d(np.arange(NpFine), fixed)
+    bFine[np.arange(0, NFine[1] * (NFine[0] + 1) + 1, NFine[0] + 1)] \
+        += bFine[np.arange(NFine[0], NpFine, NFine[0] + 1)]
+    bFine[np.arange(NFine[0] + 1)] += bFine[np.arange(NFine[1] * (NFine[0] + 1), NpFine)]
+    averageVector[np.arange(0, NFine[1] * (NFine[0] + 1) + 1, NFine[0] + 1)] \
+        += averageVector[np.arange(NFine[0], NpFine, NFine[0] + 1)]
+    averageVector[np.arange(NFine[0] + 1)] += averageVector[np.arange(NFine[1] * (NFine[0] + 1), NpFine)]
+
+    KFemFree = KFEM[free][:, free]
+    constraint = averageVector[free].reshape((1, KFemFree.shape[0]))
+    K = sparse.bmat([[KFemFree, constraint.T],
+                     [constraint, None]], format='csc')
+    bFree = bFine[free] - faverage * averageVector[free]  # right-hand side with non-zero average potentially not working correctly yet
+    b = np.zeros(K.shape[0])
+    b[:np.size(bFree)] = bFree
+    x = sparse.linalg.spsolve(K, b)
+    uFree = x[:np.size(bFree)]
+
+    uFEM = np.zeros(NpFine)
+    uFEM[free] = uFree
+
+    uFEM[np.arange(NFine[0], NpFine - 1, NFine[0] + 1)] \
+        += uFEM[np.arange(0, NFine[1] * (NFine[0] + 1), NFine[0] + 1)]
+    uFEM[np.arange(NFine[1] * (NFine[0] + 1), NpFine)] += uFEM[np.arange(NFine[0] + 1)]'''
+
+
     #true LOD
-    middle = world.NWorldCoarse[1] // 2 * world.NWorldCoarse[0] + world.NWorldCoarse[0] // 2 #2d!!!
+    #middle = world.NWorldCoarse[1] // 2 * world.NWorldCoarse[0] + world.NWorldCoarse[0] // 2 #2d!!!
+    middle = NCoarse[0] // 2
     patchRef = PatchPeriodic(world, k, middle)
     IPatch = lambda: interp.L2ProjectionPatchMatrix(patchRef)
     computeKmsijT = lambda TInd: computeKmsij(TInd, aPert, IPatch)
@@ -127,9 +172,39 @@ for N in range(NSamples):
     uFull, _ = pglod.solvePeriodic(world, KFull, bFull, faverage, boundaryConditions)
     uLodCoarse = basis * uFull
 
-    error = np.sqrt(np.dot(uLodCoarse-uLodCoarsepert, MFull*(uLodCoarse-uLodCoarsepert)))
-    print("L2-error in {}th sample is: {}".format(N, error))
+    L2norm = np.sqrt(np.dot(uLodCoarsepert, MFull * uLodCoarsepert))
+    error = np.sqrt(np.dot(uLodCoarse-uLodCoarsepert, MFull*(uLodCoarse-uLodCoarsepert))/L2norm)
+    #L2normref = np.sqrt(np.dot(uFEM, MFull * uFEM))
+    #error_ref = np.sqrt(np.dot(uLodCoarse - uFEM, MFull * (uLodCoarse - uFEM)) / L2normref)
+    print("L2-error in {}th sample between LOD approaches is: {}".format(N, error))
+    #print("L2-error in {}th sample to FEM ref sol is: {}".format(N, error_ref))
     mean_error += error
+
+'''    fig = plt.figure()
+    ax1 = fig.add_subplot(1, 3, 1)
+    ax2 = fig.add_subplot(1, 3, 2)
+    ax3 = fig.add_subplot(1, 3, 3)
+
+    uLodCoarse_pertGrid = uLodCoarsepert.reshape(world.NWorldFine+1, order='C')
+    uLodCoarseGrid = uLodCoarse.reshape(world.NWorldFine+1, order='C')
+    uFineGrid = uFEM.reshape(world.NWorldFine+1, order='C')
+
+    im3 = ax3.imshow(uFineGrid, origin='lower_left',\
+                     extent=(xpFine[:, 0].min(), xpFine[:, 0].max(), xpFine[:, 1].min(), xpFine[:, 1].max()), cmap=plt.cm.hot)
+    fig.colorbar(im3, ax=ax3)
+
+    im1 = ax1.imshow(uLodCoarse_pertGrid, origin='lower_left',\
+                     extent=(xpFine[:, 0].min(), xpFine[:, 0].max(), xpFine[:, 1].min(), xpFine[:, 1].max()), cmap=plt.cm.hot)
+    fig.colorbar(im1, ax=ax1)
+
+    im2 = ax2.imshow(uLodCoarseGrid, origin='lower_left',\
+                     extent=(xpFine[:,0].min(), xpFine[:,0].max(), xpFine[:,1].min(), xpFine[:,1].max()), cmap=plt.cm.hot)
+    fig.colorbar(im2, ax=ax2)
+
+    #im3 = ax3.imshow(aPertgrid, origin= 'lower_left',\
+    #                 extent=(xp[:, 0].min(), xp[:, 0].max(), xp[:, 1].min(), xp[:, 1].max()), cmap=plt.cm.hot)
+    #fig.colorbar(im6, ax=ax6)
+    plt.show()'''
 
 mean_error /= NSamples
 print("mean L2-error over {} samples is: {}".format(NSamples, mean_error))
